35ff053fe0cfc649c5efd061ee4a2835b0279fad
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-09 12:54:16 -0300

    Update Consulta.java

--- Consulta.java
+++ Consulta.java
@@ -1,1 +1,33 @@
+import java.util.Date;
 
+public class Consulta{
+
+	private Veterinario veterinario;
+	private Date date;
+	private String hora;
+	
+	public Consulta(Veterinario veterinario, Date date, String hora) {
+		this.veterinario = veterinario;
+		this.date = date;
+		this.hora = hora;
+	}
+	public Veterinario getVeterinario() {
+		return veterinario;
+	}
+	public void setVeterinario(Veterinario veterinario) {
+		this.veterinario = veterinario;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getHora() {
+		return hora;
+	}
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	
+}
