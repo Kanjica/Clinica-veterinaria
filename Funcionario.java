3dc31704bd8be0c3bfbdc7eef603309157db0495
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-09 12:52:03 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,1 +1,56 @@
+public class Funcionario{
 
+	private String nome;
+	private String cpf;
+	private String email;
+	private int telefoneContato;
+	private String turnoTrabalho;
+	
+	public Funcionario(String nome, String cpf, String email, int telefoneContato, String turnoTrabalho) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.telefoneContato = telefoneContato;
+		this.turnoTrabalho = turnoTrabalho;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getTelefoneContato() {
+		return telefoneContato;
+	}
+
+	public void setTelefoneContato(int telefoneContato) {
+		this.telefoneContato = telefoneContato;
+	}
+
+	public String getTurnoTrabalho() {
+		return turnoTrabalho;
+	}
+
+	public void setTurnoTrabalho(String turnoTrabalho) {
+		this.turnoTrabalho = turnoTrabalho;
+	}
+}
