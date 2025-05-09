8bdc4842c3fc5627cb7d176780a9994774523b87
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-09 00:18:55 -0300

    Update Vacina.java

--- Vacina.java
+++ Vacina.java
@@ -1,1 +1,40 @@
+import java.util.Date;
 
+public class Veterinario{
+	 
+private String nomeVacina;
+private Date dataMinistrada;
+private Date validadeVacina;
+
+	public Veterinario(String nomeVacina, Date dataMinistrada, Date validadeVacina) {
+		super();
+		this.nomeVacina = nomeVacina;
+		this.dataMinistrada = dataMinistrada;
+		this.validadeVacina = validadeVacina;
+	}
+
+	public String getNomeVacina() {
+		return nomeVacina;
+	}
+
+	public void setNomeVacina(String nomeVacina) {
+		this.nomeVacina = nomeVacina;
+	}
+
+	public Date getDataMinistrada() {
+		return dataMinistrada;
+	}
+
+	public void setDataMinistrada(Date dataMinistrada) {
+		this.dataMinistrada = dataMinistrada;
+	}
+
+	public Date getValidadeVacina() {
+		return validadeVacina;
+	}
+
+	public void setValidadeVacina(Date validadeVacina) {
+		this.validadeVacina = validadeVacina;
+	}
+
+}
