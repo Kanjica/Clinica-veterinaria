709eab373757e9cce28c7be3d0e6a6def2b703f8
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-09 12:50:11 -0300

    Update Prontuario.java

--- Prontuario.java
+++ Prontuario.java
@@ -1,1 +1,47 @@
+public class Prontuario{
 
+	private Consulta consulta;
+	private String problema;
+	private String diagnostico;
+	private String medicamento;
+	
+	public Prontuario(Consulta consulta, String problema, String diagnostico, String medicamento) {
+		super();
+		this.consulta = consulta;
+		this.problema = problema;
+		this.diagnostico = diagnostico;
+		this.medicamento = medicamento;
+	}
+
+	public Consulta getConsulta() {
+		return consulta;
+	}
+
+	public void setConsulta(Consulta consulta) {
+		this.consulta = consulta;
+	}
+
+	public String getProblema() {
+		return problema;
+	}
+
+	public void setProblema(String problema) {
+		this.problema = problema;
+	}
+
+	public String getDiagnostico() {
+		return diagnostico;
+	}
+
+	public void setDiagnostico(String diagnostico) {
+		this.diagnostico = diagnostico;
+	}
+
+	public String getMedicamento() {
+		return medicamento;
+	}
+
+	public void setMedicamento(String medicamento) {
+		this.medicamento = medicamento;
+  }
+}
