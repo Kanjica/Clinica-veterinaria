5aa47236ec6365320836feb4d2155e7bea838311
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-09 12:56:37 -0300

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,1 +1,57 @@
+public class Animal{
+	
+	private String nome;
+	private String raca;
+	private String dataNascimento;
+	private Tutor tutor;
+	private Historico historico;
+	
+	public Animal(String nome, String raca, String dataNascimento, Tutor tutor, Historico historico) {
+		this.nome = nome;
+		this.raca = raca;
+		this.dataNascimento = dataNascimento;
+		this.tutor = tutor;
+		this.historico = historico;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRaca() {
+		return raca;
+	}
+
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Tutor getTutor() {
+		return tutor;
+	}
+
+	public void setTutor(Tutor tutor) {
+		this.tutor = tutor;
+	}
+
+	public Historico getHistorico() {
+		return historico;
+	}
+
+	public void setHistorico(Historico historico) {
+		this.historico = historico;
+	}
+	
+}
