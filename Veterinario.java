ff8c6fe533d4a0a63a8e8231eff5b9b5441d7184
João Pedro <165161332+Kanjica@users.noreply.github.com>
2025-05-09 00:15:01 -0300

    Update Veterinario.java

--- Veterinario.java
+++ Veterinario.java
@@ -1,2 +1,69 @@
+package Lista3LP2Faculdade;
 
- 
+public class Veterinario{
+	 
+private String nome;
+private String cpf;
+private String email;
+private String telefoneContato;
+private String especialidade;
+private int cfmv;
+
+	public Veterinario(String nome, String cpf, String email, String telefoneContato, String especialidade, int cfmv) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.telefoneContato = telefoneContato;
+		this.especialidade = especialidade;
+		this.cfmv = cfmv;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getTelefoneContato() {
+		return telefoneContato;
+	}
+	
+	public void setTelefoneContato(String telefoneContato) {
+		this.telefoneContato = telefoneContato;
+	}
+	
+	public String getEspecialidade() {
+		return especialidade;
+	}
+	
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+	
+	public int getCfmv() {
+		return cfmv;
+	}
+	
+	public void setCfmv(int cfmv) {
+		this.cfmv = cfmv;
+	}
+}
